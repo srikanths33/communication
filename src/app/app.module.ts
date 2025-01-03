487db48698de26eb84cf574c58c0acaa92d9bcd3
srikanthcherry4@gmail.com <srikanthcherry4@gmail.com>
2025-01-03 11:26:35 +0530

    app cretaed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
